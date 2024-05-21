59c38566082652262ad7108e95006bf504205e73
hitmanbuzz <moirangthemhenthoiba30@gmail.com>
2024-05-21 10:07:35 +0530

    remove icon from here to build.rs

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,15 @@ use crate::source::readm::readm;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Grabber Made By Moirangthem Henthoiba\n");
+    
+
+    println!("Grabber Made By Hitman|Moirangthem Henthoiba\n");
     print!("Enter Comic URL: ");
     stdout().flush().unwrap();
     let mut title = String::new();
     io::stdin().read_line(&mut title).unwrap();
     let title = title.trim();
     let _ = readm(&title).await;
-
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        res.set_icon("path/to/your/icon.ico");
-        res.compile().expect("Failed to set icon");
-    }
     
     Ok(())
 }
