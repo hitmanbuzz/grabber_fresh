366390fbeaa64c89c6cd265221a177ae5d55b970
hitmanbuzz <moirangthemhenthoiba30@gmail.com>
2024-05-24 17:23:19 +0530

    same as previous one without image format

--- src/source/readm.rs
+++ src/source/readm.rs
@@ -68,7 +68,6 @@ pub async fn readm(url: &str) -> Result<(), Box<dyn std::error::Error>> {
     let title: Vec<&str> = url.split("/manga/").collect();
     let title = title[1];
     let mut response = fetch_comic_chapter(url.to_owned()).await.unwrap();
-    let mut file_format = ".jpg";
 
     response.reverse();
     let set: HashSet<_> = response.into_iter().collect();
